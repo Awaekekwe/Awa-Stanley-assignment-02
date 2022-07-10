92c0b24825376fb094193e222b1b88e85a4addd2
Stanley Awa <54400045+Awaekekwe@users.noreply.github.com>
2022-07-10 16:10:12 +0000

    i attempted challenge 1&2

--- src/assignment.js
+++ src/assignment.js
@@ -13,11 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  function sumOfNumbers(arrayOfNumbers) {
+  let arr = 0;
+  for(let i = 0; i < arrayOfNumbers.length; i++)
+    {
+      arr += arrayOfNumbers[i];
+    }
+  return arr;
+    // return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
-
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -27,10 +32,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  function countEvenNumbers(arrayOfNumbers) {
+  let num = 0;
+  for(let i = 0; i < arrayOfNumbers.length; i++)
+    {
+      if(arrayOfNumbers[i]%2==0) {
+        num++;
+      }
+    }
+  // console.log(num)
+    // return arrayOfNumbers.length;
+return num
+// countEvenNumbers(n)
+    // return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
